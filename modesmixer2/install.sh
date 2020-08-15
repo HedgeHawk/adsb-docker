f58b049190eccaf8a0ee083411387656d4d04440
HedgeHawk <darth-sidious@gmx.net>
2020-08-15 17:41:21 +0200

    Update install.sh

--- modesmixer2/install.sh
+++ modesmixer2/install.sh
@@ -4,7 +4,7 @@ echo Architecture: $ARCH
 
 if [ "$ARCH" == "x86_64" ]; then 
 	echo Downloading x86_64
-	curl -SL "https://drive.google.com/uc?export=download&id=1QzUqYTt-Bf-m817-i3W_RvodB019kxC5" | tar -xzC /usr/bin modesmixer2
+	curl -SL "https://drive.google.com/uc?export=download&id=1BH7zPxzk7qTMhhGOD6KPHMnM7qRf4_Dc" | tar -xzC /usr/bin modesmixer2
 else 
 	echo Downloading armv7
 	curl -SL "https://drive.google.com/uc?export=download&id=1YvXOE7tlhtMocmQQBZ7VtW8AN_3oPleF" | tar -xzC /usr/bin modesmixer2
